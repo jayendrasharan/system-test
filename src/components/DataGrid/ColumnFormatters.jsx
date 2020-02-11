ac40eca0103df341712c389dc237dbe94354bdbd
ajayg415 <ajay.g415@gmail.com>
2020-02-11 22:25:32 +0530

    Added datatable component

---
+++ src/components/DataGrid/ColumnFormatters.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PriorityFormatter = props => {
+  return <span className='priority'>{props.row.priority}</span>
+};
+
+
+export { 
+  PriorityFormatter
+}
--- src/components/DataGrid/DataGrid.jsx
+++ src/components/DataGrid/DataGrid.jsx
@@ -1,29 +1,89 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import DataTable from "react-data-table-component";
+
+import { deleteRow } from './../../store/actions/actions'
 import DataRow from './DataRow'
+import CustomModal from './../CustomModal/CustomModal';
+
+import { 
+  PriorityFormatter
+} from './ColumnFormatters';
 
 import './DataGrid.css';
 
 const DataGrid = props =>{
-  const { todoList } = props;
+  const { todoList, removeRow } = props;
+
+  const columns = [
+    {
+      name: "Summary",
+      selector: "summary",
+      sortable: true,
+    },
+    {
+      name: "Priority",
+      selector: "priority",
+      sortable: true,
+      cell : row => <PriorityFormatter row={row} />
+    },
+    {
+      name: "Created On",
+      selector: "createdAt",
+      sortable: true,
+    },
+    {
+      name: "Due Date",
+      selector: "dueDate",
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      selector: "",
+      cell : row => {
+        console.log(row);
+        const { currentState } = row;
+        return (
+          <>
+            <CustomModal type="edit" title="Edit Todo" row={row} />
+            <CustomModal type="view" title="View Todo" row={row} />
+            <i
+              className="fa mx-2 fa-trash"
+              aria-hidden="true"
+              onClick={() => removeRow(row)}
+            ></i>
+            <input
+              type="button"
+              className="btn btn-sm btn-link"
+              value={currentState ? 'Done' : 'Re-Open'}
+            />
+          </>
+        );
+      }
+    }
+  ]
+
+  const conditionalRowStyles = [
+    {
+      when: row => !row.currentState,
+      style: {
+        backgroundColor: '#defade',
+      },
+    }
+  ]
   return (
-    <table className='table table-stripped table-hover table-sm table-bordered'>
-      <thead>
-        <tr>
-          <th>Summary <i className="fa fa-arrow-down" aria-hidden="true"></i></th>
-          <th>Priority</th>
-          <th>Created On</th>
-          <th>Due date</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {todoList.map(row=>{
-          return <DataRow key={row.id} row={row}/>
-        })}
-      </tbody>
-    </table>
+    <DataTable
+      title="TODo List"
+      columns={columns}
+      data={todoList}
+      fixedHeader
+      dense
+      fixedHeaderScrollHeight="350px"
+      highlightOnHover
+      className='todolist-component'
+      conditionalRowStyles={conditionalRowStyles}
+    />
   );
 };
 
@@ -31,4 +91,8 @@ const mapStateToProps = state =>({
   todoList: state.combReducer.todos
 })
 
-export default connect(mapStateToProps, null)(DataGrid);
+const mapDispatchToProps = dispatch =>({
+  removeRow: row => dispatch(deleteRow(row))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataGrid);
