548562afda293c2e0c1d26370c4939a2db907550
Durga Prasad <Durga.Prasad@Vitality.co.uk>
2020-03-11 14:48:53 +0530

    Changes has been done

---
+++ src/Sample.js
@@ -0,0 +1,52 @@
+const data =
+    [
+        {
+            "id": 1,
+            "firstName": "John",
+            "lastName": "Mathew",
+            "age": 10,
+            "gender": "Male",
+            "blood_group": "B+"
+        },
+        {
+            "id": 2,
+            "firstName": "Alice",
+            "lastName": "Cruz",
+            "age": 12,
+            "gender": "Female",
+            "blood_group": "A+"
+        },
+        {
+            "id": 3,
+            "firstName": "Senorita",
+            "lastName": "Raise",
+            "age": 15,
+            "gender": "Female",
+            "blood_group": "A-"
+        },
+        {
+            "id": 4,
+            "firstName": "Tony",
+            "lastName": "Hirat",
+            "age": 8,
+            "gender": "Male",
+            "blood_group": "AB+"
+        },
+        {
+            "id": 5,
+            "firstName": "Krish",
+            "lastName": "Rajpara",
+            "age": 16,
+            "gender": "Female",
+            "blood_group": "O+"
+        },
+        {
+            "id": 6,
+            "firstName": "Hridhan",
+            "lastName": "Swami",
+            "age": 12,
+            "gender": "Male",
+            "blood_group": "AB-"
+        }
+    ]
+export default data;
