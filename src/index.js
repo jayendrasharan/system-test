72df83b20a68d214c3feb7a4289600d8a0a734f9
gowthamip1805 <65488455+gowthamip1805@users.noreply.github.com>
2020-05-17 15:27:37 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--
+		** Todo-Sortable-List Example App
+		** Licensed under the Apache License v2.0
+		** http://www.apache.org/licenses/LICENSE-2.0
+		** Built by Kenneth Hu 
+		-->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="React js Todo-Sortable-List Example" />
+    <meta name="author" content="Kenneth Hu" />
+    <title>Kenneth's the first reactJs</title>
+    <link rel="stylesheet" href="css/libs/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/libs/jquery-ui.min.css" />
+    <link rel="stylesheet" href="css/common.css" />
+    <link rel="stylesheet" href="css/base.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.1/react.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.0.1/react-dom.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.16/browser.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.min.js"></script>
+    <script src="js/libs/analytics.js"></script>
+    <script type="text/babel" src="js/app.js"></script>
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header"></div>
+
+        <!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container" id="playground">
+      <h1>Reactjs-Todo List</h1>
+      <div id="todo"></div>
+    </div>
+  </body>
+</html>
