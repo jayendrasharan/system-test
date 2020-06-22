a181db9c3b415917c66efd5909936959073243fd
kapilpatidar94 <kapilpatidar94@gmail.com>
2020-06-22 13:41:36 +0530

    all-actions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import { Task } from './Components'
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
   render() {
     return (
+
       <div className="App">
-        <h1 className="logo">To-Do-App</h1>
-        <span class="addNew">+</span>
+        <header className="App-header">
+          <h1 className="App-title">To-Do-App</h1>
+        </header>
+
+        <div className="row">
+          <div className="column1">
+          </div>
+          <div className="column2">
+            <Task />
+          </div>
+          <div className="column3">
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { default as AddTask } from './Task/AddTask'
+export { default as TaskList } from './Task/TaskList'
+export { default as Modal } from './Modal'
+export { default as Task } from './Task'
+export { default as Highlight } from './Highlight'
